9438eed86175074a98c1cf41f8bf15407bb5a8e9
Rizqi Siti Inayah 2428250080P <rizqisitiinayah_2428250080p@mhs.mdp.ac.id>
2025-04-11 20:31:39 +0700

    gempa dan cuaca latihan

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch('https://24252-if21-pw1-omega.vercel.app/javascript/wilayah.json')
 .then( data => {
     console.log(data);
     data.forEach( (item) => {
-        document.getElementById("list-desa").innerHTML += `<li onclick='detail("${item.kode}")'> ${item.kode} ${item.nama} </li>`
+        document.getElementById("list-desa").innerHTML += `<li class="list-group-item" onclick='detail("${item.kode}")'> ${item.kode} ${item.nama} </li>`
     })
 })
  function detail(kode) {
@@ -22,8 +22,31 @@ fetch('https://24252-if21-pw1-omega.vercel.app/javascript/wilayah.json')
         data.data[0].cuaca.forEach( (item) => {
             console.log(item);
             item.forEach( (cuaca) => {
-            document.getElementById("list-cuaca").innerHTML += `${cuaca.weather_desc} `
+            document.getElementById("list-cuaca").innerHTML += 
+// margin itu sisi luar/tabelnya, padding sisi dalam/cardnya
+            `<div class="col-lg-3 p-2">
+            <div class="card">
+            <img src="${cuaca.image}" class="card-img-top p-2" alt="...">
+            <div class="card-body">
+                <h6 class="card-title">${cuaca.weather_desc}</h6>
+                <p class="card-text">${cuaca.utc_datetime}</p>
+            </div>
+            </div>
+            </div>`
             })
         })
     })
  }
+
+//  Akses API data Gempa BMKG
+fetch('https://data.bmkg.go.id/DataMKG/TEWS/gempaterkini.json')
+.then( respone => response.json()) 
+.then( data => {
+    // console.log(data)
+    console.log(data);
+    data.Infogempa.gempa.forEach( (data) => {
+        document.getElementById('wilayah').innerHTML = data.lokasi.Wilayah`<li>
+        ${data.Wilayah} ${data.Tanggal} ${data.Jam} ${data.Potensi}
+        </li>`
+    })
+})
